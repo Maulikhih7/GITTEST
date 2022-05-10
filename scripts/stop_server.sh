f940656334fe19069d1663eedcbbc907c4ce340a
HIh7 Webtech <46366354+Maulikhih7@users.noreply.github.com>
2022-05-10 15:29:54 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 isExistApp=`pgrep httpd`
 if [[ -n  $isExistApp ]]; then
-#     sudo service httpd stop
-    /sbin/service httpd stop    
+    sudo service httpd stop
+#     /sbin/service httpd stop    
 fi
